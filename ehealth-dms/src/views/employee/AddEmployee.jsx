9b7ce2455f29a7a385dff06f682a5db83575dd21
Ridwan Abiola <ricklick262@gmail.com>
2020-09-15 15:34:02 +0100

    :hammer: fix Strings must use doublequote, Expected === and instead saw ==, Unexpected console statement

--- ehealth-dms/src/views/employee/AddEmployee.jsx
+++ ehealth-dms/src/views/employee/AddEmployee.jsx
@@ -1,23 +1,22 @@
-import React, { Component } from 'react';
-import DateFnsUtils from '@date-io/date-fns';
-import Avatar from '@material-ui/core/Avatar/Avatar';
-import Typography from '@material-ui/core/Typography/Typography';
-import PersonAddIcon from '@material-ui/icons/PersonAdd';
-import Container from '@material-ui/core/Container/Container';
-import { makeStyles } from '@material-ui/core/styles';
-import { Usestyles } from '../../components';
-import Grid from '@material-ui/core/Grid';
-import TextField from '@material-ui/core/TextField/TextField';
+import React, { Component } from "react";
+import DateFnsUtils from "@date-io/date-fns";
+import Avatar from "@material-ui/core/Avatar/Avatar";
+import Typography from "@material-ui/core/Typography/Typography";
+import PersonAddIcon from "@material-ui/icons/PersonAdd";
+import Container from "@material-ui/core/Container/Container";
+import { Usestyles } from "../../components";
+import Grid from "@material-ui/core/Grid";
+import TextField from "@material-ui/core/TextField/TextField";
 import {
   FormControlLabel,
   Checkbox,
   Button,
   MenuItem,
-} from '@material-ui/core';
+} from "@material-ui/core";
 import {
   KeyboardDatePicker,
   MuiPickersUtilsProvider,
-} from '@material-ui/pickers';
+} from "@material-ui/pickers";
 
 class AddEmployee extends Component {
   render() {
@@ -34,28 +33,28 @@ function RegisterForm() {
 
   const employeeType = [
     {
-      value: 'doctor',
-      label: 'Doctor',
+      value: "doctor",
+      label: "Doctor",
     },
     {
-      value: 'nurse',
-      label: 'Nurse',
+      value: "nurse",
+      label: "Nurse",
     },
     {
-      value: 'pa',
-      label: 'PA',
+      value: "pa",
+      label: "PA",
     },
     {
-      value: 'socialWorker',
-      label: 'Social Worker',
+      value: "socialWorker",
+      label: "Social Worker",
     },
     {
-      value: 'administrator',
-      label: 'Administrator',
+      value: "administrator",
+      label: "Administrator",
     },
     {
-      value: 'receptionist',
-      label: 'Receptionist',
+      value: "receptionist",
+      label: "Receptionist",
     },
   ];
 
@@ -67,19 +66,18 @@ function RegisterForm() {
   const handleSelectChange = (event) => {
     let stateValue = event.target.name;
 
-    if (stateValue == 'employeeSelect') {
+    if (stateValue === "employeeSelect") {
       setEmployee(event.target.value);
     }
-    if (stateValue == 'genderSelect') {
+    if (stateValue === "genderSelect") {
       setGender(event.target.value);
     }
-    if (stateValue == 'statusSelect') {
+    if (stateValue === "statusSelect") {
       setStatus(event.target.value);
     }
   };
 
   const handleDateChange = (date) => {
-    console.log('@@@@@: ', date);
     setSelectedDate(date);
   };
 
@@ -102,7 +100,7 @@ function RegisterForm() {
                 select
                 fullWidth
                 size="medium"
-                value={employee ? employee : 'doctor'}
+                value={employee ? employee : "doctor"}
                 defaultValue="doctor"
                 onClick={handleSelectChange}
                 helperText="Select employee type"
@@ -114,7 +112,6 @@ function RegisterForm() {
                   </MenuItem>
                 ))}
               </TextField>
-              <div>{employee}</div>
             </Grid>
             <Grid item xs={12} sm={6}>
               <TextField
@@ -123,7 +120,7 @@ function RegisterForm() {
                 select
                 fullWidth
                 size="medium"
-                value={gender ? gender : 'male'}
+                value={gender ? gender : "male"}
                 onClick={handleSelectChange}
                 helperText="Select Gender"
                 variant="outlined"
@@ -157,7 +154,7 @@ function RegisterForm() {
                 autoComplete="Department"
               />
             </Grid>
-           
+
             <Grid item xs={12}>
               <TextField
                 variant="outlined"
@@ -189,7 +186,7 @@ function RegisterForm() {
                 select
                 fullWidth
                 size="medium"
-                value={status ? status : 'permanent'}
+                value={status ? status : "permanent"}
                 onClick={handleSelectChange}
                 helperText="Select Employment status"
                 variant="outlined"
@@ -202,7 +199,7 @@ function RegisterForm() {
             <Grid item xs={12} sm={12}>
               <MuiPickersUtilsProvider utils={DateFnsUtils}>
                 <KeyboardDatePicker
-                  views={['year']}
+                  views={["year"]}
                   margin="normal"
                   fullWidth
                   variant="inline"
@@ -214,7 +211,7 @@ function RegisterForm() {
                   value={selectedDate}
                   onChange={handleDateChange}
                   KeyboardButtonProps={{
-                    'aria-label': 'change date',
+                    "aria-label": "change date",
                   }}
                 />
               </MuiPickersUtilsProvider>
